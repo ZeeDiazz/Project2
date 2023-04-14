3421fd3420995eb0145ca3512d87685b87cf94c9
Zigalow <s224302@dtu.dk>
2023-04-14 13:40:30 +0200

    Added readFile code and header

---
+++ readFile.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "readFile.h"
+
+int readFromFile(char *filename) {
+    const int bufferSize = 156;
+
+    FILE *pFile = fopen("cards.txt", "r");
+    char buffer[bufferSize];
+
+    if(pFile == NULL){
+        return -1;
+    }
+
+    while (fgets(buffer, bufferSize, pFile) != NULL) {
+
+    }
+    fclose(pFile);
+
+
+
+}
+
+Deck loadDeck(char* filename){
+
+}
+
+
+
+Deck convertCardCard (char*){
+}
