6ef19dc77de045239cc8ea7c4be424970c79d20d
ZEE <Zaidsh02@gmail.com>
2023-04-28 09:33:53 +0200

    linkedList removeCard and the rest updated

--- linkedList.c
+++ linkedList.c
@@ -4,38 +4,82 @@
 #include "linkedList.h"
 
 
-
-void addCard(Node* node, Card card){
-    Node* testNode = malloc(sizeof(Node));
-    testNode->card = card;
-    node->next = testNode;
+LinkedList* makeEmptyList(){
+    LinkedList* list = malloc(sizeof(LinkedList));
+    list->head = NULL;
+    list->size = 0;
+    return list; //return a pointer to the new empty list
 }
-void addFirst(Node* node, Card card){
+
+/**
+ * This function add a card at the beginning of the list.
+ * @author ZedDiazz (Zaid)
+ * @param list
+ * @param card
+ */
+void addCard(LinkedList* list, Card card){
+    Node* node = malloc(sizeof(Node));
+    node->card = card;
+
+    list->head = node;
+    list->size++;
+}
+/**
+ * @author ZedDiazz (Zaid)
+ * @param list
+ * @param card
+ */
+
+void addFirst(LinkedList* list, Card card){
     //allocate memory
-    Node* testNode = malloc(sizeof(Node));
-    testNode->card = card;
-    testNode->next = node->next;
-    node->next = testNode;
+    Node* node = malloc(sizeof(Node));
+    node->card = card;
+
+    node->next = list->head->next;
+
+    list->head = node;
+    list->size++;
 }
 
-void addLast(Node* node, Card card){
-    Node* testNode = malloc(sizeof(Node));
-    testNode->card = card;
+void addLast(LinkedList* list, Card card){
+    Node* node = malloc(sizeof(Node));
+    node->card = card;
+    node->next = NULL; //Null since its the last element
 
-    while (node->next != NULL){
-        node->next = testNode;
+    while (list->head != NULL){
+        list->head = node;
     }
 }
 
-void remove(Node* node, Card card){
 
+void removeCard(LinkedList* list, Card card) {
+    Node *temp = list->head;
+    Node *prev = NULL;
+
+    while (temp != NULL) {
+        if (temp->card.value == card.value && temp->card.suit == card.suit) {
+            list->head->next = temp->next;
+            free(temp);
+            list->size--; //since we are removing
+            return; // to stop the while loop
+        }
+        prev->card = temp->card;
+        temp = temp->next; //iterate through temp
+    }
 }
 
-void printList(Node* node){
+Card getCardAt(LinkedList* list, int index){
+    //a for loop
+    for(int i = 0; i < index;i++){
+
+    }
+}
+
+void printList(LinkedList* list){
     //while (node){
     //}
 }
 
-bool isEmpty(Node* node) {
-    return node == NULL;
+bool isEmpty(LinkedList* list) {
+    return list->head == NULL;
 }
