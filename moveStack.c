10008f6255cb155e29a03da0c432541c7ee522b9
Zigalow <s224302@dtu.dk>
2023-04-28 12:05:42 +0200

    Added moveStack.c and moveStack.h

---
+++ moveStack.c
@@ -0,0 +1,5 @@
+//
+// Created by zegho on 28-04-2023.
+//
+
+#include "moveStack.h"
---
+++ moveStack.h
@@ -0,0 +1,8 @@
+//
+// Created by zegho on 28-04-2023.
+//
+
+#ifndef PROJECT2_MOVESTACK_H
+#define PROJECT2_MOVESTACK_H
+
+#endif //PROJECT2_MOVESTACK_H
