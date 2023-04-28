b9316cf7dcbce80580efc5b1f7452e7dc78fbbfb
Daniel Weper Jensen <danielweper@gmail.com>
2023-04-28 14:43:18 +0200

    Added start of the game logic in main

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include "card.h"
+#include "cli.h"
+#include "commands.h"
+#include "game.h"
+#include "linkedList.h"
+#include "moveStack.h"
+#include "moveValidation.h"
+#include "readFile.h"
+#include "shuffleDeck.h"
 
 int main() {
+    Game game = {STARTUP, 0, 0, NULL};
+    Command quitCommand = {QQ, false, ""};
+    while (game.phase != QUITTING) {
+        useCommand(&game, quitCommand);
+    }
     printf("Hello, World!\n");
     return 0;
 }
